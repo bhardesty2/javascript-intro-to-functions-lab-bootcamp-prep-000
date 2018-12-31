d76bf97413d9f0a85bc962d5f9dee8c2ac8004f6
Blake Hardesty <ha1003218@email.ccbcmd.edu>
2018-12-31 19:02:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,14 +8,5 @@ function whisper(string){
   return string.toLowerCase();
 }
 
-function sayHiToGrandma(string) {
-  if ((string)=== toUpperCase) {
-    return 'YES INDEED!'
-  } 
-  else if ((string)=== toLowerCase) {
-    return "I can't hear you!"
-  }
-  else if ((string)==="I love you, Grandma") {
-    return "I love you, too."
-  }
-}
+function shout(string)
+console.log 
